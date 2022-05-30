ed5abcec77c9483c5d7b42a76980d5d973c00ccc
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-05-30 13:34:07 +0300

    linking the restaurant-details API to  restaurant-reviews JS

---
+++ JS/restaurant-details-and-reviews.js
@@ -0,0 +1,55 @@
+window.onload = function () {
+  let restaurant_details = document.getElementById("restaurant-details");
+  let url_string = window.location.href;
+  let url = new URL(url_string);
+  let user_id = url.searchParams.get("user_id");
+  let restaurant_id = url.searchParams.get("restaurant_id");
+  console.log(user_id);
+
+  axios({
+    method: "get",
+    url: `http://localhost/Project3-Zomato%20-Back%20-%20End/zomato-back-end/APIs/retaurant-details.php?user_id=${user_id}&restaurant_id=${restaurant_id}`,
+  }).then(function (response) {
+    let details = response.data;
+    console.log(details);
+    let restaurant_details_section = document.createElement("section");
+    restaurant_details_section.id = "restaurant-details";
+    restaurant_details_section.innerHTML = `<div class="restaurant-images page-content">
+      <div class="restaurant-image-box">
+      <img src="../assets/mcdonalds.jpg" alt="">
+      </div>
+      <div class="restaurant-image-box">
+          <img src="../assets/mcdonalds.jpg" alt="">
+      </div>
+      <div class="restaurant-image-box">
+          <img src="../assets/mcdonalds.jpg" alt="">
+      </div>
+  </div>
+  <div class="restaurant-details page-content">
+      <div class="restaurant-details-box">
+          <div class="restaurant-main">
+              <div class="restaurant-main-details">
+                  <div class="restaurant-logo">
+                      <img src="../assets/mcdonalds.jpg" alt="">
+                  </div>  
+                  <div class="restaurant-headers">
+                      <h1> ${details.restaurant_name} </h1>
+                      <h2> ${details.category_name} </h2>
+                      <h2> ${details.location} </h2>
+                  </div>
+              </div>
+              <div class="rating">
+                  <i class="fa-solid fa-star"></i> ${details.restaurant_rating} 
+              </div>
+          </div>
+          <p> ${details.restaurant_description} </p>
+          <h3> average cost</h3>
+          <p>
+              ${details.average_cost}$ for two people (appprox) 
+          </p>
+      </div>
+        </div>`;
+    body = document.getElementsByTagName("body")[0];
+    body.appendChild(restaurant_details_section);
+  });
+};
