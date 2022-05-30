e9219d0f73a4717915596365cc2123fb947bd467
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-05-30 11:33:15 +0300

    creating the adminDisplayReviews JS file | deleting the pagination and dummy data in adminReviews HTML file

---
+++ JS/adminDisplayReviews.js
@@ -0,0 +1,38 @@
+window.onload = function () {
+  var review_display = document.getElementById("reviews-display");
+
+  axios({
+    method: 'post',
+    url: 'http://localhost/zomato-back-end/APIs/admin-display-reviews.php',
+  }).then(function (response) {
+
+    var reviews = response.data
+    for (var i = 0; i < reviews.length; i++) {
+
+      var status;
+      if (reviews[i]["status"] == 1) {
+        status = "accept"
+      }
+      if (reviews[i]["status"] == 2) {
+        status = "reject"
+      }
+
+      console.log(i);
+      const card = document.createElement("tr");
+      card.innerHTML =
+        `<td><img src="image/user.png" class="tab-img"></td>
+      <td>${reviews[i]["name"]}</td>
+      <td><img src="image/user.png" class="tab-img"></td>
+      <td>${reviews[i]["first_name"]}</td>
+      <td>${reviews[i]["last_name"]}</td>
+      <td>${reviews[i]["rating"]}</td>
+      <td>${reviews[i]["content"]}</td>
+      <td>${reviews[i]["date_posted"]}</td>
+      <td>${status}</td>`
+
+
+      review_display.appendChild(card);
+    }
+
+  })
+}
