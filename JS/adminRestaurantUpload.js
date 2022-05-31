3efbfa38cdea24f380b61ceb8eef4ff12e546dc4
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-05-31 22:00:20 +0300

    fixing an image issue in uploading restaurants

--- JS/adminRestaurantUpload.js
+++ JS/adminRestaurantUpload.js
@@ -6,13 +6,21 @@ window.onload = function () {
   let description = document.getElementById('description');
   let category = document.getElementById('category');
   let profile = document.getElementById('image-profile');
-  let logo = document.getElementById('logo-profile');
 
   document.getElementById('submit-btn').addEventListener("click", submitFct);
+//   document.getElementById('submit-btn').addEventListener("click", function(){
+//     restaurant_name.value= "";
+//     location.value= "";
+//     average_cost.value= "";
+//     description.value= "";
+//     // category.value= "";
+//     // profile.value= "";
+// }
+    // );
 
   function submitFct() {
     if (restaurant_name.value == "" || location.value == "" || average_cost.value == "" || description.value == ""
-      || profile.files.length == 0 || logo.files.length == 0) {
+      || profile.files.length == 0  ) {
       // the images are empty arrays, its not a string 
       alert("Fill all elements please");
     }
@@ -40,7 +48,6 @@ window.onload = function () {
         var category_id = category.options[category.selectedIndex].value;
         mydata.append("category_id", category_id);
         mydata.append("image", base64String);
-        mydata.append("logo", logo.value);
 
         
         axios({
